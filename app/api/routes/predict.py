58934fe202e5d94dac39f483773c840ea20cfcb0
Cristian <cristian.vcabezas@hotmail.com>
2026-04-18 19:00:53 -0500

    endpoint stats

--- app/api/routes/predict.py
+++ app/api/routes/predict.py
@@ -21,3 +21,27 @@ async def history(conn = Depends(get_db)):
     from app.services.comments_service import get_predictions
     return await get_predictions(conn)
 
+@router.get("/stats/count")
+async def get_stats(conn=Depends(get_db)):
+
+    rows = await conn.fetch("""
+        SELECT
+            result,
+            COUNT(*) AS total
+        FROM predictions
+        GROUP BY result
+        ORDER BY total DESC
+    """)
+
+    # convertir a JSON serializable
+    data = [
+        {
+            "result": row["result"],
+            "total": row["total"]
+        }
+        for row in rows
+    ]
+
+    return {
+        "stats": data
+    }
