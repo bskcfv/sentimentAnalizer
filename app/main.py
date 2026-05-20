16bf4cb9537be216bfe2dcf1b859ee0d51066f90
Cristian <cristian.vcabezas@hotmail.com>
2026-05-19 19:21:25 -0500

    update CORS

--- app/main.py
+++ app/main.py
@@ -10,7 +10,8 @@ app = FastAPI(
 
 origins = [
     "http://localhost:3000",  
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://sentiment-analyzer-frontend-murex.vercel.app"
 ]
 
 app.add_middleware(
